076234b2e03335d093c439a71292bcaa9250d017
Oleg Somov <doremi@fasollasi.ru>
2024-07-26 20:56:05 +0300

    Added BankTest

--- src/test/java/ru/netology/javaqadiplom/BankTest.java
+++ src/test/java/ru/netology/javaqadiplom/BankTest.java
@@ -6,19 +6,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BankTest {
-
+/*
     @Test
     public void test() {
-        CreditAccount account1 = new CreditAccount(1_000,5_000,15);
+        SavingAccount account1 = new SavingAccount(1_000,500,15_000, 5);
         SavingAccount account2 = new SavingAccount(1_000,1_000,10_000,5);
         Bank Abank = new Bank();
         int difference1 = account1.getBalance() - account2.getBalance(); //0
 
         Abank.transfer(account1, account2, 100);
 
-        int difference2 = account1.getBalance() - account2.getBalance(); // 900 - 1100 = 200
+        int difference2 = account2.getBalance() - account1.getBalance(); // 900 - 1100 = -200
 
-        Assertions.assertEquals(difference2, 200);
+        Assertions.assertEquals(difference2, );
     }
-
+*/
 }
