83d795f490edc9b31f86090ca3c672c70f1c13c8
Oleg Somov <doremi@fasollasi.ru>
2024-07-26 14:28:48 +0300

    Debugging SangAccount

---
+++ src/main/java/ru/netology/javaqadiplom/IllegalArgumentException.java
@@ -0,0 +1,7 @@
+package ru.netology.javaqadiplom;
+
+public class IllegalArgumentException extends RuntimeException {
+    public IllegalArgumentException(String msg) {
+        super(msg);
+    }
+}
